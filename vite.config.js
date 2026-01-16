94e47e935391d0d6f37d6caae40c40eeafd04365
Keshan Panditharathna <keshanebusiness@gmail.com>
2026-01-17 01:40:30 +0530

    Fixed vite config base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nwd-clothing/", // 👈 මේ පේළිය අනිවාර්යයෙන්ම එකතු කරන්න (Repo එකේ නම)
+  base: "/nwd-clothing/", // 👈 මේ කොටස තමයි වැදගත්ම!
 })
